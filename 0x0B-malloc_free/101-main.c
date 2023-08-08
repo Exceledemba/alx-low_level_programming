33f0c2c59b141fc68927937786db3de389585e51
Exceledemba <edembauche@gmail.com>
2023-08-08 15:20:08 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -1,12 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-char **strtow(char *);
-
 /**
  * print_tab - Prints an array of string
- *
  * @tab: The array to print
+ *
+ * Return: nothing
  */
 void print_tab(char **tab)
 {
@@ -14,70 +14,25 @@ void print_tab(char **tab)
 
 	for (i = 0; tab[i] != NULL; ++i)
 	{
-		printf("%s\n", tab[i]);
+	printf("%s\n", tab[i]);
 	}
 }
 
 /**
- * main - check the code .
+ * main - check the code for ALX School students.
  *
- * Return: Always 0.
+ * Return: 1 if an error occurred, 0 otherwise
  */
 int main(void)
 {
 	char **tab;
-	int i;
 
-	tab = strtow("      Talk        is     cheap.       Show      me     the    code.        ");
+	tab = strtow("      ALX School         #cisfun      ");
 	if (tab == NULL)
 	{
-		printf("Failed\n");
-		return (1);
+	printf("Failed\n");
+	return (1);
 	}
 	print_tab(tab);
-	for (i = 0; tab[i] != NULL; i++)
-		free(tab[i]);
-
-
-	tab = strtow("Talk is cheap. Show me the code.");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	for (i = 0; tab[i] != NULL; i++)
-		free(tab[i]);
-
-	tab = strtow(" !@#$^&*(())_Talk ><><.., ][{} 76478 is ~~ cheap :) . Show #$45&*(45 me 873589 the |[]{} -_=+ code.^& ");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	for (i = 0; tab[i] != NULL; i++)
-		free(tab[i]);
-
-	tab = strtow(" ");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	for (i = 0; tab[i] != NULL; i++)
-		free(tab[i]);
-
-	tab = strtow("");
-	if (tab == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	print_tab(tab);
-	for (i = 0; tab[i] != NULL; i++)
-		free(tab[i]);
-
 	return (0);
 }
