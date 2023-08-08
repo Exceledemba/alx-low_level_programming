d21d2645b81bc2653ec3b9e530ed08843a1a04b3
Exceledemba <edembauche@gmail.com>
2023-08-08 14:17:53 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - test _strdup
+ * main - check the code for ALX School students.
  *
  * Return: Always 0.
  */
@@ -11,50 +11,13 @@ int main(void)
 {
 	char *s;
 
-	s = _strdup("ALX is awesome!");
+	s = _strdup("ALX SE");
 	if (s == NULL)
 	{
-		printf("failed to allocate memory\n");
-		return (1);
+	printf("failed to allocate memory\n");
+	return (1);
 	}
 	printf("%s\n", s);
 	free(s);
-
-	s = _strdup("Holberton School!");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-	s = _strdup("");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-	s = _strdup(NULL);
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
-	s = _strdup("First, solve the problem. Then, write the code.");
-	if (s == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	printf("%s\n", s);
-	free(s);
-
 	return (0);
 }
